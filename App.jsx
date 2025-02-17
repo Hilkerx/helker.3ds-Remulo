11f286cdab651086ba8a335eb676dad4e2b6fdf1
Hilkerx <166817216+Hilkerx@users.noreply.github.com>
2025-02-17 09:43:24 -0300

    AppJSX.17/02

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import HelloWorld from "./HelloWorld"
+import Contador from "./contador";
+import Mensagem from "./mensagem";
+function App () {
+    return (
+        <div>
+        <HelloWorld />
+        <Contador />
+        <Mensagem />
+        </div>
+    );
+}
